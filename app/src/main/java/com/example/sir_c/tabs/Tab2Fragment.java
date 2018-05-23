2b664680099a9d7fd6f3e955bbaee212a5a5c983
raul-Toledo <36417948+raul-Toledo@users.noreply.github.com>
2018-05-23 12:03:53 -0600

    Se agrego funcionalidad al segundo tab

--- app/src/main/java/com/example/sir_c/tabs/Tab2Fragment.java
+++ app/src/main/java/com/example/sir_c/tabs/Tab2Fragment.java
@@ -7,9 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Tab2Fragment extends Fragment {
+
+    EditText editText;
+    Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,27 @@ public class Tab2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        final View view = inflater.inflate(R.layout.fragment_tab2, container, false);
+        if (view!=null){
+            editText = (EditText) view.findViewById(R.id.edtNumero);
+            button = (Button)view.findViewById(R.id.btnToast);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String strMensaje ="";
+                    if (!editText.getText().toString().isEmpty()){
+                        strMensaje = editText.getText().toString();
+                    } else{
+                        strMensaje = "Cadena vacia";
+                    }
+                    Toast.makeText(getContext(), strMensaje, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        return view;
+
     }
 
 }
